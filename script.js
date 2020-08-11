e0417251caec1beded5d592ececeb88c2dc523b2
H Lowe <haleylowe0@gmail.com>
2020-08-11 14:37:55 -0700

    set searchInput to localStorage. began if statement for onclick event

--- script.js
+++ script.js
@@ -2,11 +2,49 @@ let city = "Sacramento";
 //let state = "CA";
 let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ba3476e23f33fcbced7a77c27b3df244";
 
+
+
+//searchBtn function
+$("#searchBtn").click(function() { 
+    click.preventDefault();
+    let searchInput = $("#searchInput"); 
+    let searchCity = $(this).siblings(searchInput).val();
+    localStorage.setItem(searchCity, JSON.stringify(searchInput));
+    console.log(searchInput);
+    console.log(setCity);
+})
+//End searchBtn function
+
+if ($("#searchBtn").click === true) {
+
+}
+
+
+
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
+//begin function
 }).then(function(response){
 console.log(response);
 
+//begin if statement
+if (response.list == "2", "10", "18", "26", "34") {
+    console.log(response.list[2].main);
+}
+//end if statement
+
 });
+//end function
+
+
+//whatever is in the top [global scope] will load
+//8 per day for 5 day for loop
+//if dt_txt = 15:00 00
+//add new value to create new buttons
+//add event listener as container
+//the target is event.target
+//if statement for when someone comes to page for the first time
 
